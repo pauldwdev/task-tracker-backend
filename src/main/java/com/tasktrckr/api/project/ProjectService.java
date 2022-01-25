34d1c18ed6d29e2ce2ff5f2e84d0491e2829c337
paul <paul@PaulDW-LPTP.hsd1.nj.comcast.net>
2022-01-25 06:22:55 -0500

    Added getAllProjects endpoint.

--- src/main/java/com/tasktrckr/api/project/ProjectService.java
+++ src/main/java/com/tasktrckr/api/project/ProjectService.java
@@ -1,7 +1,11 @@
 package com.tasktrckr.api.project;
 
+import java.util.List;
+
 import com.tasktrckr.api.dto.project.ProjectResponseDto;
 
 public interface ProjectService {
 	public ProjectResponseDto getProject(Integer projectId);
+
+	public List<ProjectResponseDto> getProjects();
 }
--- src/main/java/com/tasktrckr/api/project/ProjectServiceImpl.java
+++ src/main/java/com/tasktrckr/api/project/ProjectServiceImpl.java
@@ -1,5 +1,7 @@
 package com.tasktrckr.api.project;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
@@ -27,6 +29,12 @@ public class ProjectServiceImpl implements ProjectService {
 			throw new ResponseStatusException(HttpStatus.NOT_FOUND, "project not found");
 		}
 		return projectMapper.toProjectResponseDto(project);
+	}
+
+	@Override
+	public List<ProjectResponseDto> getProjects() {
+		return projectMapper.toProjectResponseDtoList(projectRepository.findAll());
+
 	}
 
 }
