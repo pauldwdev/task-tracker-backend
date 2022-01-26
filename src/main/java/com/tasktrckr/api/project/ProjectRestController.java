b118002d16e582a1acb3ba014433963b30b8ef1f
paul <paul@PaulDW-LPTP.hsd1.nj.comcast.net>
2022-01-26 06:43:59 -0500

    Added additional endpoints.

--- src/main/java/com/tasktrckr/api/project/ProjectRestController.java
+++ src/main/java/com/tasktrckr/api/project/ProjectRestController.java
@@ -4,13 +4,19 @@ import java.util.List;
 
 import org.jboss.logging.MDC;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.tasktrckr.api.dto.project.ProjectRequestDto;
 import com.tasktrckr.api.dto.project.ProjectResponseDto;
+import com.tasktrckr.api.dto.task.TaskRequestDto;
 import com.tasktrckr.api.dto.task.TaskResponseDto;
 import com.tasktrckr.api.task.TaskRestController;
 import com.tasktrckr.api.task.TaskService;
@@ -48,7 +54,7 @@ public class ProjectRestController {
 		MDC.remove(MDC_CONTEXT);
 		return project;
 	}
-	
+
 	@Operation(summary = "Get all projects")
 	@ApiResponses(value = { @ApiResponse(responseCode = "200", description = "Found projects to return", content = {
 			@Content(mediaType = "application/json", array = @ArraySchema(schema = @Schema(implementation = ProjectResponseDto.class))) }) })
@@ -60,5 +66,42 @@ public class ProjectRestController {
 		log.info("--== END getProjects");
 		MDC.remove(MDC_CONTEXT);
 		return projectList;
+	}
+
+	@PutMapping(produces = "application/json")
+	@Operation(summary = "Update a project")
+	@ApiResponses(value = { @ApiResponse(responseCode = "200", description = "Updated and return project", content = {
+			@Content(mediaType = "application/json", schema = @Schema(implementation = ProjectResponseDto.class)) }) })
+	public @ResponseBody ProjectResponseDto updateProject(@RequestBody ProjectRequestDto projectRequestDto) {
+		MDC.put(MDC_CONTEXT, PROJECT_MDC);
+		log.info("--== START updateProject");
+		ProjectResponseDto project = projectService.updateProject(projectRequestDto);
+		log.info("--== END updateProject");
+		MDC.remove(MDC_CONTEXT);
+		return project;
+	}
+
+	@PostMapping(produces = "application/json")
+	@Operation(summary = "Create a Project")
+	@ApiResponses(value = { @ApiResponse(responseCode = "200", description = "Create and return project", content = {
+			@Content(mediaType = "application/json", schema = @Schema(implementation = ProjectResponseDto.class)) }) })
+	public @ResponseBody ProjectResponseDto createProject(@RequestBody ProjectRequestDto projectRequestDto) {
+		MDC.put(MDC_CONTEXT, PROJECT_MDC);
+		log.info("--== START createProject");
+		ProjectResponseDto project = projectService.createProject(projectRequestDto);
+		log.info("--== END createProject");
+		MDC.remove(MDC_CONTEXT);
+		return project;
+	}
+
+	@DeleteMapping(path = "/{projectId}", produces = "application/json")
+	@Operation(summary = "Delete a project")
+	@ApiResponses(value = { @ApiResponse(responseCode = "200", description = "Delete project") })
+	public void deleteProject(@PathVariable int projectId) {
+		MDC.put(MDC_CONTEXT, PROJECT_MDC);
+		log.info("--== START deleteProject");
+		projectService.deleteProject(projectId);
+		log.info("--== END deleteProject");
+		MDC.remove(MDC_CONTEXT);
 	}
 }
